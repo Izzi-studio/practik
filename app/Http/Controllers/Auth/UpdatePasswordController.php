87ef61ee305b40acd33266bb308f3b426dc8bcbb
izzi-dev <devops@izzi.studio>
2021-12-03 13:48:22 +0200

    update pwd refactor

--- app/Http/Controllers/Auth/UpdatePasswordController.php
+++ app/Http/Controllers/Auth/UpdatePasswordController.php
@@ -59,12 +59,12 @@ class UpdatePasswordController extends Controller
             'new_password' => ['required'],
             'new_confirm_password' => ['same:new_password'],
         ]);
-   
-        User::find(auth()->user()->id)->update(['password'=> Hash::make($request->new_password)]);
-   
+        auth()->user()->update(['password'=> Hash::make($request->new_password)]);
+        //User::find(auth()->user()->id)->update(['password'=> Hash::make($request->new_password)]);
+
         return view('auth.passwords.update')->with('success','Successful applicant')
         ->with(['additional_info'=>User::getAdittionalInfo()]);
-        
+
     }
 
     /**
